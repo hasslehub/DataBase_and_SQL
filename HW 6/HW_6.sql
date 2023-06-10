94a8dab216f6b0ea7380bbc0259c7bea4c97eb4e
hasslehub <66742510+hasslehub@users.noreply.github.com>
2023-06-10 22:18:33 +0300

    Add files via upload

--- HW 6/HW_6.sql
+++ HW 6/HW_6.sql
@@ -1,26 +1,120 @@
-CREATE DATABASE IF NOT EXISTS home_work_6;
-USE home_work_6;
+-- CREATE DATABASE IF NOT EXISTS home_work_6;
+USE lesson_4;
 
+
+-- Создайте таблицу users_old, аналогичную таблице users. 
+-- Создайте процедуру, с помощью которой можно переместить любого (одного) пользователя из таблицы users в таблицу users_old. 
+-- (использование транзакции с выбором commit или rollback – обязательно).
+
+DROP TABLE IF EXISTS users_old;
+CREATE TABLE users_old (
+	id INT PRIMARY KEY auto_increment, 
+    firstname varchar(50), 
+    lastname varchar(50), 
+    email varchar(120)
+);
+
+
+DROP PROCEDURE IF EXISTS replace_user;
 DELIMITER $$
-CREATE PROCEDURE times(seconds INT)
+CREATE PROCEDURE  replace_user (IN num1 INT) 
+	DETERMINISTIC
+BEGIN
+INSERT INTO users_old (firstname,lastname,email) 
+SELECT firstname, lastname, email 
+	FROM users 
+	WHERE users.id = num1;
+DELETE FROM users 
+	WHERE id = num1;
+COMMIT;
+END$$
+
+DELIMITER ;
+
+CALL replace_user(4); 
+
+
+
+
+-- Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. 
+-- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", 
+-- с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+DROP FUNCTION IF EXISTS hello;
+DELIMITER $$
+CREATE FUNCTION hello() 
+	RETURNS VARCHAR(25)
+	DETERMINISTIC
+BEGIN
+DECLARE result_text VARCHAR(25);
+SELECT CASE 
+	WHEN CURRENT_TIME >= '12:00:00' AND  CURRENT_TIME < '18:00:00' THEN 'Добрый день'
+	WHEN CURRENT_TIME >= '06:00:00' AND  CURRENT_TIME < '12:00:00' THEN 'Доброе утро'
+	WHEN CURRENT_TIME >= '00:00:00' AND  CURRENT_TIME < '06:00:00' THEN 'Доброй ночи'
+	ELSE 'Добрый вечер'
+END INTO result_text;
+RETURN result_text;
+END$$
+
+DELIMITER ;
+
+SELECT hello();
+
+
+ -- (по желанию)* Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, 
+ -- communities и messages в таблицу logs помещается время и дата создания записи, название 
+ -- таблицы, идентификатор первичного ключа.
+
+
+DROP TABLE IF EXISTS logs;
+
+CREATE TABLE logs (
+    created_at DATETIME DEFAULT now(),
+    table_name VARCHAR(20) NOT NULL,
+    pk_id INT UNSIGNED NOT NULL
+)  
+ENGINE=ARCHIVE;
+
+DROP TRIGGER IF EXISTS users_log;
+CREATE TRIGGER  users_log
+AFTER INSERT ON users FOR EACH ROW 
+    INSERT INTO logs SET table_name = 'users' , pk_id = NEW.id;
+
+DROP TRIGGER IF EXISTS communities_log;
+CREATE TRIGGER  communities_log
+AFTER INSERT ON communities FOR EACH ROW 
+    INSERT INTO logs SET table_name = 'communities' , pk_id = NEW.id;
+
+DROP TRIGGER IF EXISTS messages_log;
+CREATE TRIGGER  messages_log
+AFTER INSERT ON messages FOR EACH ROW 
+    INSERT INTO logs SET table_name = 'messages' , pk_id = NEW.id;
+
+
+-- Создайте функцию, которая принимает кол-во сек и формат их в кол-во дней, часов, минут и секунд.
+-- Пример: 123456 ->'1 days 10 hours 18 minutes 36 seconds '
+
+DROP PROCEDURE IF EXISTS times;
+DELIMITER $$
+CREATE PROCEDURE times(IN seconds INT)
 BEGIN
     DECLARE days INT default 0;
     DECLARE hours INT default 0;
     DECLARE minutes INT default 0;
 
-    WHILE seconds >= 84600 DO
-    SET days = seconds / 84600;
-    SET seconds = seconds % 84600;
+    WHILE seconds >= (60 * 60 * 24) DO
+		SET days = seconds / (60 * 60 * 24);
+		SET seconds = seconds % (60 * 60 * 24);
     END WHILE;
 
-    WHILE seconds >= 3600 DO
-    SET hours = seconds / 3600;
-    SET seconds = seconds % 3600;
+    WHILE seconds >= (60 * 60) DO
+		SET hours = seconds / (60 * 60);
+		SET seconds = seconds % (60 * 60);
     END WHILE;
 
     WHILE seconds >= 60 DO
-    SET minutes = seconds / 60;
-    SET seconds = seconds % 60;
+		SET minutes = seconds / 60;
+		SET seconds = seconds % 60;
     END WHILE;
 
 SELECT days, hours, minutes, seconds;
@@ -30,32 +124,30 @@ DELIMITER ;
 CALL times(123456);
 
 
--- 2. Выведите только четные числа от 1 до 10 включительно. (Через функцию / процедуру)
--- Пример: 2,4,6,8,10 (можно сделать через шаг +  2: х = 2, х+=2)
-DROP FUNCTION IF EXISTS get_nums;
-DELIMITER $$
-CREATE FUNCTION get_nums()
-RETURNS VARCHAR(50) DETERMINISTIC 
-BEGIN
-	DECLARE result VARCHAR(50) DEFAULT "";
-	DECLARE start_value INT DEFAULT 2;
-	DECLARE stop_value INT DEFAULT 10;
-    DECLARE n INT DEFAULT 0;
-	SET n = IF(start_value % 2 = 0, start_value, start_value + 1);
-	IF n <= stop_value THEN 
-		REPEAT
-			IF LENGTH(result) > 0 THEN
-				SET result = CONCAT(result, ",");
-            END IF;
-			SET result = CONCAT(result, n);
-			SET n = n + 2;
-			UNTIL n > stop_value
-		END REPEAT;
-	END IF;
-	RETURN result;
-END $$
+-- Выведите только четные числа от 1 до 10 (Через цикл)
+-- Пример: 2,4,6,8,10
 
-SELECT get_nums();
+DROP FUNCTION IF EXISTS even_integer; 
+DELIMITER $$ 
+CREATE FUNCTION even_integer(n INT) 
+RETURNS VARCHAR(50)
+DETERMINISTIC 
+
+BEGIN 
+	DECLARE i INT DEFAULT 0; 
+	DECLARE result VARCHAR(50);
+	SET result=''; 
+	WHILE i < n DO 
+		SET i = i + 2; 
+		SET result = concat(result, '  ', i); 
+	END WHILE; 
+RETURN result; 
+END $$ 
+DELIMITER; 
+
+SELECT even_integer(10);
+
+
 
 
 -- 1. Создать процедуру, которая решает следующую задачу
@@ -63,14 +155,11 @@ SELECT get_nums();
 -- а) из одного города
 -- б) состоят в одной группе
 -- в) друзья друзей	
-DROP PROCEDURE IF EXISTS users_5;
-DELIMITER //
-CREATE PROCEDURE users_5
-(
-	IN id_user_find INT
-)
-BEGIN
-	
+
+DROP PROCEDURE IF EXISTS users_list;
+DELIMITER $$
+CREATE PROCEDURE users_list(IN user_find INT)
+BEGIN	
     SELECT t.id
     FROM
     (
@@ -78,13 +167,13 @@ BEGIN
 		FROM users u
 		INNER JOIN profiles p
 		ON u.id = p.user_id
-		AND u.id <> id_user_find
+		AND u.id <> user_find
 		AND (
 			SELECT p1.hometown
 			FROM users u1
 			INNER JOIN profiles p1
 			ON u1.id = p1.user_id
-			AND u1.id = id_user_find
+			AND u1.id = user_find
 		) = p.hometown
 		UNION    
 		SELECT DISTINCT u.id 
@@ -92,28 +181,25 @@ BEGIN
 		INNER JOIN users_communities uc
 		ON u.id = uc.user_id
 		WHERE uc.community_id IN 
-		(
-			SELECT community_id
-			FROM users_communities
-			WHERE users_communities.user_id = id_user_find
-		)    
+			(SELECT community_id
+				FROM users_communities
+				WHERE users_communities.user_id = user_find)    
 		UNION
 		SELECT id
 		FROM users 
 		WHERE users.id IN (
-			(
-				SELECT initiator_user_id AS id 
+			(SELECT initiator_user_id AS id 
 				FROM friend_requests
 				WHERE status='approved' 
 				AND target_user_id IN (
 					SELECT initiator_user_id AS id 
 					FROM friend_requests
-					WHERE target_user_id = id_user_find AND status='approved'
+					WHERE target_user_id = user_find AND status='approved'
 					UNION ALL
 					SELECT target_user_id 
 					FROM friend_requests
-					WHERE initiator_user_id = id_user_find AND status='approved'
-				) 
+					WHERE initiator_user_id = user_find AND status='approved') 
+                    
 				UNION
 				SELECT target_user_id 
 				FROM friend_requests
@@ -121,25 +207,26 @@ BEGIN
 				AND initiator_user_id IN (
 					SELECT initiator_user_id AS id 
 					FROM friend_requests
-					WHERE target_user_id = id_user_find AND status='approved'
+					WHERE target_user_id = user_find AND status='approved'
 					UNION ALL
 					SELECT target_user_id 
 					FROM friend_requests
-					WHERE initiator_user_id = id_user_find AND status='approved'
-				)
+					WHERE initiator_user_id = user_find AND status='approved')
 			)
 		)
 	) t
     ORDER BY RAND() 
     LIMIT 5;
 
-END//
+END $$
+DELIMITER;
 
-CALL users_5(4);
+CALL users_list(4);
 
 -- 2. Создать функцию, вычисляющей коэффициент популярности пользователя
+
 DROP FUNCTION IF EXISTS get_popularity_coefficient;
-DELIMITER //
+DELIMITER $$
 CREATE FUNCTION get_popularity_coefficient(
 	user_id INT
 )
@@ -163,13 +250,14 @@ BEGIN
     WHERE u.id = user_id;
     
     RETURN result;
-END//
+END $$
 
 SELECT get_popularity_coefficient(1); 
 
--- 3. Создать процедуру для добавления нового пользователя с профилем
+--  Создать процедуру для добавления нового пользователя с профилем
+
 DROP PROCEDURE IF EXISTS add_new_user;
-DELIMITER //
+DELIMITER $$
 CREATE PROCEDURE add_new_user(
 	IN firstname VARCHAR(50),
     IN lastname VARCHAR(50),
@@ -207,7 +295,7 @@ BEGIN
     VALUES
     (user_id, gender, birthday, media_id, hometown);
     
-END//
+END $$
 
 CALL add_new_user("test", "test", "test", "m", '2011-12-20', "test", "test", "test", @user_id);
 
@@ -219,57 +307,3 @@ LEFT JOIN media
 ON profiles.photo_id = media.id
 AND users.id = media.user_id
 WHERE users.id = @user_id;
-
-
-
--- Создайте хранимую функцию hello(), которая будет возвращать приветствие, 
--- в зависимости от текущего времени суток. С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", 
--- с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", 
--- с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
-
-DELIMITER $$
-CREATE FUNCTION hello() 
-	RETURNS VARCHAR(25)
-	DETERMINISTIC
-BEGIN
-DECLARE result_text VARCHAR(25);
-SELECT CASE 
-	WHEN CURRENT_TIME >= '12:00:00' AND  CURRENT_TIME < '18:00:00' THEN 'Добрый день'
-	WHEN CURRENT_TIME >= '06:00:00' AND  CURRENT_TIME < '12:00:00' THEN 'Доброе утро'
-	WHEN CURRENT_TIME >= '00:00:00' AND  CURRENT_TIME < '06:00:00' THEN 'Доброй ночи'
-	ELSE 'Добрый вечер'
-END INTO result_text;
-RETURN result_text;
-END$$
-
-DELIMITER ;
-SELECT hello();
-
-
-
- -- (по желанию) - Создайте таблицу logs типа Archive. 
- -- Пусть при каждом создании записи в таблицах users, communities и messages 
- -- в таблицу logs помещается время и дата создания записи, название таблицы, идентификатор первичного ключа.
- 
-DROP TABLE IF EXISTS logs;
-
-CREATE TABLE logs (
-    created_at DATETIME DEFAULT now(),
-    table_name VARCHAR(20) NOT NULL,
-    pk_id INT UNSIGNED NOT NULL
-)  ENGINE=ARCHIVE;
-
-CREATE 
-    TRIGGER  users_log
- AFTER INSERT ON users FOR EACH ROW 
-    INSERT INTO logs SET table_name = 'users' , pk_id = NEW.id;
-
-CREATE 
-    TRIGGER  communities_log
- AFTER INSERT ON communities FOR EACH ROW 
-    INSERT INTO logs SET table_name = 'communities' , pk_id = NEW.id;
-
-CREATE 
-    TRIGGER  messages_log
-AFTER INSERT ON messages FOR EACH ROW 
-    INSERT INTO logs SET table_name = 'messages' , pk_id = NEW.id;
